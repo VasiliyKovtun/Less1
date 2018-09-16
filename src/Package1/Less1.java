9bad92af4dfef2a1e8f0114ecbcce3ac44c05aa5
Vasiliy Kovtun <vkovtun82@gmail.com>
2018-09-16 10:50:10 +0300

    create patch

--- src/Package1/Less1.java
+++ src/Package1/Less1.java
@@ -6,6 +6,7 @@ public class Less1 {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("Hello, world!");
+        System.out.println("Hello, world!");
 
 
     }
